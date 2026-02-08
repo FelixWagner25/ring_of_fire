138e0a6c451a9c061fb0f12d98ede72e5070b4e8
Felix Wagner <post@felixwagner.eu>
2026-02-08 14:19:36 +0100

    navigate to game component onclick of start game button

--- src/app/start-screen/start-screen.ts
+++ src/app/start-screen/start-screen.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-start-screen',
   imports: [],
   templateUrl: './start-screen.html',
   styleUrl: './start-screen.scss',
 })
 export class StartScreen {
+  private router = inject(Router);
 
+  newGame(){
+    this.router.navigate(['game'])
+  }
 }
