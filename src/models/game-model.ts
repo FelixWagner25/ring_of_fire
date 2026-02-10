7dc04784a141771a89042765030d395f47b1882a
Felix Wagner <post@felixwagner.eu>
2026-02-10 10:46:38 +0100

    insert multiple players

--- src/models/game-model.ts
+++ src/models/game-model.ts
@@ -1,5 +1,5 @@
 export class GameModel{
-    public players:string[] = [];
+    public players:string[] = ["Hans", "Jürgen", "Dieter"];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
