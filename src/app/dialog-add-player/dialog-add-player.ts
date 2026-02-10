df51862ca5bc076318703b59027d525df1d3aa8e
Felix Wagner <post@felixwagner.eu>
2026-02-10 15:54:14 +0100

    feat: continue dialog-add-players html and ts

--- src/app/dialog-add-player/dialog-add-player.ts
+++ src/app/dialog-add-player/dialog-add-player.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Input } from '@angular/core';
+import { MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-dialog-add-player',
-  imports: [],
+  imports: [MatDialogContent, MatDialogActions, FormsModule, MatLabel, MatFormField],
   templateUrl: './dialog-add-player.html',
   styleUrl: './dialog-add-player.scss',
 })
 export class DialogAddPlayer {
 
+  @Input() name: string = "";
+
+  readonly dialogRef = inject(MatDialogRef<DialogAddPlayer>);
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
