fc62d572bbdc6ac9f9bd5fff424cf05fb1daf178
Felix Wagner <post@felixwagner.eu>
2026-02-15 17:41:43 +0100

    feat: add missing description

--- src/app/game-info/game-info.ts
+++ src/app/game-info/game-info.ts
@@ -23,9 +23,9 @@ cardAction = [
     { title: 'Chicks', description: 'All girls drink.' },
     { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
     { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
-    { title: 'Thumbmaster', description: '' },
+    { title: 'Thumbmaster', description: 'The player who drew the card must put their thumb on the table at any chosen time (before the next five gets picked though, or they lose the right). The last person to put their thumb on the table must drink. Other variations involve the Thumbmaster choosing a forfeit for the last person to do it.' },
     { title: 'Men', description: 'All men drink.' },
-    { title: 'Quizmaster', description: '' },
+    { title: 'Quizmaster', description: 'You become the question master and if anybody answerds a question asked by you (the player who dre wht card), they have to drink. This applies to ANY question.' },
     { title: 'Never have i ever...', description: 'Say something you nnever did. Everyone who did it has to drink.' },
     { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
   ];
